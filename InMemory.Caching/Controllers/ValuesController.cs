e1c6bc1df91e79d7f83409e8df99c95c33f0a83d
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-05-27 13:14:08 +0300

    Set and get method studied on In-Memory Caching example.

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		readonly IMemoryCache _memoryCache;
+
+		public ValuesController(IMemoryCache memoryCache)
+		{
+			_memoryCache = memoryCache;
+		}
+
+		[HttpGet("set/{name}")]
+		public void Set(string name)
+		{
+			_memoryCache.Set("name", name); //veri cachleme
+		}
+
+		[HttpGet]
+		public string Get()
+		{
+			//_memoryCache.Get("name"); //cachlenen veriyi okuma. sonuç object tipinde döner.
+			return _memoryCache.Get<string>("name"); //cachlenen veriyi okuma. sonuç generic olarak verilen tip türünde döner.
+		}
+		
+	}
+}
