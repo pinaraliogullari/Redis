eef2841f0e103ac003664bf3846ff0a5d1bb7c1e
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-05-27 14:46:29 +0300

    Distributed Caching is done.

---
+++ Distributed.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Distributed.Caching.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesController : ControllerBase
+	{
+		readonly IDistributedCache _distributedCache;
+
+		public ValuesController(IDistributedCache distributedCache)
+		{
+			_distributedCache = distributedCache;
+		}
+
+		[HttpGet("set")]
+		public async Task<IActionResult> Set(string name,string surname)
+		{
+			await _distributedCache.SetStringAsync("name", name); //string format
+			await _distributedCache.SetAsync("surname",Encoding.UTF8.GetBytes(surname)); //byte format
+			return Ok();
+
+		}
+
+		[HttpGet("get")]
+		public async Task<IActionResult> Get()
+		{
+			var name= await _distributedCache.GetStringAsync("name");
+			var surnameBinary = await _distributedCache.GetAsync("surname");
+			var surname= Encoding.UTF8.GetString(surnameBinary);
+			return Ok(new
+			{
+				name,
+				surname
+			});
+		}
+	}
+}
--- InMemory.Caching/Controllers/ValuesController.cs
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -15,24 +15,40 @@ namespace InMemory.Caching.Controllers
 			_memoryCache = memoryCache;
 		}
 
-		[HttpGet("set/{name}")]
-		public void Set(string name)
+		//[HttpGet("set/{name}")]
+		//public void SetName(string name)
+		//{
+		//	_memoryCache.Set("name", name); 
+		//}
+
+		//[HttpGet]
+		//public string GetName()
+		//{
+		//	//_memoryCache.Get("name"); //cachlenen veriyi okuma. sonuç object tipinde döner.
+		//	//return _memoryCache.Get<string>("name"); //cachlenen veriyi okuma. sonuç generic olarak verilen tip türünde döner.
+
+		//	//In memoryden çektiğimiz veriler üzerinde çeşitli çalışmalar yapabiliriz. Ancak uygulama kapandıktan sonra veriler bellekten silinir ve tekrar okumak istediğimizde veriye erişemez durumda oluruz , üzerinde gerçekleştireeğimiz işlem adımları sebebiyle de çalışma zamanı hataları alabiliriz. Bunu önlemek adına veriyi korunaklı bir şekilde elde etmek için aşağıdaki davranışı sergilemeliyiz. 
+		//	if(_memoryCache.TryGetValue<string>("name", out string name))
+		//		return name;
+
+		//	return "";
+
+		//}
+
+		[HttpGet("setDate")]
+		public void SetDate()
 		{
-			_memoryCache.Set("name", name); //veri cachleme
+			_memoryCache.Set<DateTime>("date", DateTime.Now, options: new()
+			{
+				AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+				SlidingExpiration=TimeSpan.FromSeconds(5)
+			});
 		}
 
-		[HttpGet]
-		public string Get()
+		[HttpGet("getDate")]
+		public DateTime GetDate()
 		{
-			//_memoryCache.Get("name"); //cachlenen veriyi okuma. sonuç object tipinde döner.
-			//return _memoryCache.Get<string>("name"); //cachlenen veriyi okuma. sonuç generic olarak verilen tip türünde döner.
-
-			//In memoryden çektiğimiz veriler üzerinde çeşitli çalışmalar yapabiliriz. Ancak uygulama kapandıktan sonra veriler bellekten silinir ve tekrar okumak istediğimizde veriye erişemez durumda oluruz , üzerinde gerçekleştireeğimiz işlem adımları sebebiyle de çalışma zamanı hataları alabiliriz. Bunu önlemek adına veriyi korunaklı bir şekilde elde etmek için aşağıdaki davranışı sergilemeliyiz. 
-			if(_memoryCache.TryGetValue<string>("name", out string name))
-				return name;
-
-			return "";
-			
+			return _memoryCache.Get<DateTime>("date");
 		}
 		
 	}
